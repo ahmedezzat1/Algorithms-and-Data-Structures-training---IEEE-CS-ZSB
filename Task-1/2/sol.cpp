9f894443bd5eda824eb1c71e36d1f8d30fb3d483
Ahmed Ezzat <65311452+ahmedezzat1@users.noreply.github.com>
2022-02-24 15:28:41 +0200

    Create sol.cpp

---
+++ Task-1/2/sol.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+    int main() {
+ 
+       double n;
+       cin>>n;
+       cout<<fixed<<setprecision(1)<<n/log1p(n)<<" " ;
+        cout<<fixed<<setprecision(1)<<1.25506*n/log1p(n)<<" " ;
+    
+}
+
