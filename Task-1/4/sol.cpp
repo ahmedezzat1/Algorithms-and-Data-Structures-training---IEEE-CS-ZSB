fa3b28d02f7107cea999a240bfbb891190146a8e
Ahmed Ezzat <65311452+ahmedezzat1@users.noreply.github.com>
2022-02-24 15:39:45 +0200

    Create sol.cpp

---
+++ Task-1/4/sol.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+    int main() {
+    file();
+    fast
+    int t = 1;
+    //cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        for (int i = 1; i <= 10; ++i) {
+            cout << i << " x " << n << " = " << i * n << endl;
+        }
+    }
+}
+
