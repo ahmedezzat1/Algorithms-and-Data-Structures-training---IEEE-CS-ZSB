a878bc5d162826f57eface36460f8a52b32b17dd
Ahmed Ezzat <65311452+ahmedezzat1@users.noreply.github.com>
2022-02-24 15:41:14 +0200

    Create sol.cpp

---
+++ Task-1/6/sol.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+    int main() {
+    file();
+    fast
+    int t =1;
+    cin >> t;
+    while (t--) {
+        string s;
+      getline(cin ,s);
+      cout<<"gzuz"<<endl;
+    }
+}
